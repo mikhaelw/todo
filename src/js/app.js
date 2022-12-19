1934c617ce976b2cc20657bd49e548bc67ded327
Mikhael White <structura-nova@ukr.net>
2022-12-19 19:18:05 +0200

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from "./modules/functions.js";
+import * as lazyLoad from "./modules/lazyload.js";
+import * as materialize from "./modules/materialize.js";
+
+flsFunctions.isWebp();
+lazyLoad.lazyLoad();
+materialize.materialize();
